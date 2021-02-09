4de80c4a98100c7a21af7abb582d3800fa7cd5e3
Silvian <silvian.dragan@gmail.com>
2021-02-09 21:38:47 +0000

    fixed the number formatting function logic

--- utils.py
+++ utils.py
@@ -4,9 +4,13 @@ from datetime import datetime
 def fix_number_formatting(number):
     if number:
         number = str(number)
+        number = number.strip().replace(" ", "")
         if not number.startswith('+'):
             if not number.startswith('0'):
                 return "0" + number
+        else:
+            if number.find("0") == 3:
+                return number[:3] + number[4:]
 
     return number
 
